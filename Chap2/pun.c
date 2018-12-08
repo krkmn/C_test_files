81c99c77ba252e60ae0fc1fd4ce1e8e781deb1bd
krkmn <owais.sulehria@gmail.com>
2018-12-08 15:18:22 +0000

    Add files from VPS

---
+++ Chap2/pun.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+
+printf("Jag är sanningen");
+printf("\nny rad\n");
+
+float F;
+F = 0.1; //Provar om det blir en warning om jag inte har med f, vilket det inte blev
+
+return 0;
+}
